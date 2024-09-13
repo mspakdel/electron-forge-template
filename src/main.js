188cdf33528f360c35095ae8a8278fb0fa69c9ab
mspakdel <ms.pakdel@gmail.com>
2024-09-13 11:26:45 +0300

    this is a template for create electron with forge application

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "main.html"));
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(async () => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
